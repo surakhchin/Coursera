c4396a8179e981bb311caa0f4e6297353f57eed7
Serge <urakhchin@gmail.com>
2017-03-07 22:43:45 -0800

    Assignment 2 Contd: Figured out how to use radio buttons and bind javascript to it, simply use ng-model and set value of the radio button.

--- conFusion/app/scripts/app.js
+++ conFusion/app/scripts/app.js
@@ -161,18 +161,19 @@ angular.module('confusionApp',[])
     .controller('DishCommentController', ['$scope', function($scope) {
 
             //Step 1: Create a JavaScript object to hold the comment from the form
-            $scope.mycomment =  {yourname: "",
-                                 yourrating: "",
-                                 yourcomment: ""
+            $scope.mycomment =  {author: "",
+                                 rating: "5",
+                                 comment: "",
+                                 date: ""
             };
 
             $scope.submitComment = function () {
 
                 //Step 2: This is how you record the date
-                // "The date property of your JavaScript object holding the comment" = new Date().toISOString();
+                $scope.mycomment.date = new Date().toISOString();
 
                 // Step 3: Push your comment into the dish's comment array
-                $scope.dish.comments.push("Your JavaScript Object holding the comment");
+                $scope.dish.comments.push(mycomment);
 
                 //Step 4: reset your form to pristine
 
