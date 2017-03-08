3703f92a9804f72729a7606e4956a704892481ae
Serge <urakhchin@gmail.com>
2017-03-07 21:25:10 -0800

    Course 3: Angular Assignment 2: Angular Form Validation and Form set up. TODO: Need to create javascript object  to store comment data.

--- conFusion/app/scripts/app.js
+++ conFusion/app/scripts/app.js
@@ -1,113 +1,185 @@
-
-        angular.module('confusionApp',[])
+angular.module('confusionApp',[])
 //        now we add the controller using app
-        .controller('MenuController',['$scope',function ($scope) {
-            $scope.tab = 1;
-            //select function that gets called by ng-click, this select function
-            //goes and changes the tab value based on what tab number was given by ng-click
-            //ex:if user clicks on 2nd tab, tab variable will be equal to two
+    .controller('MenuController',['$scope',function ($scope) {
+        $scope.tab = 1;
+        //select function that gets called by ng-click, this select function
+        //goes and changes the tab value based on what tab number was given by ng-click
+        //ex:if user clicks on 2nd tab, tab variable will be equal to two
+        $scope.showDetails = false;
+        $scope.filtText = '';
+        //we create the dishes object here within the menuController
+        $scope.dishes=[
+             {
+               name:'Uthapizza',
+               image: 'images/uthapizza.png',
+               category: 'mains',
+               label:'Hot',
+               price:'4.99',
+               description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+               comment: ''
+            },
+            {
+               name:'Zucchipakoda',
+               image: 'images/zucchipakoda.png',
+               category: 'appetizer',
+               label:'',
+               price:'1.99',
+               description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+               comment: ''
+            },
+            {
+               name:'Vadonut',
+               image: 'images/vadonut.png',
+               category: 'appetizer',
+               label:'New',
+               price:'1.99',
+               description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
+               comment: ''
+            },
+            {
+               name:'ElaiCheese Cake',
+               image: 'images/elaicheesecake.png',
+               category: 'dessert',
+               label:'',
+               price:'2.99',
+               description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+               comment: ''
+            }
+            ];
+        //Angular Filter Code not whenever we add code we tend to initialize the main variable being used
+        $scope.select = function(setTab) {
+            $scope.tab = setTab;
 
-            $scope.isSelected = function (checkTab) {
-                return ($scope.tab === checkTab);
-            };
-            //Angular Filter Code not whenever we add code we tend to initialize the main variable being used
-            $scope.showDetails = false;
-            $scope.filtText = '';
-            $scope.select = function(setTab) {
-                $scope.tab = setTab;
+            if (setTab === 2)
+                $scope.filtText = "appetizer";
+            else if (setTab === 3)
+                $scope.filtText = "mains";
+            else if (setTab === 4)
+                $scope.filtText = "dessert";
+            else
+                $scope.filtText = "";
+        };
 
-                if (setTab === 2)
-                    $scope.filtText = "appetizer";
-                else if (setTab === 3)
-                    $scope.filtText = "mains";
-                else if (setTab === 4)
-                    $scope.filtText = "dessert";
-                else
-                    $scope.filtText = "";
-            };
-            //we create the dishes object here within the menuController
-           $scope.dishes=[
-                 {
-                   name:'Uthapizza',
-                   image: 'images/uthapizza.png',
-                   category: 'mains',
-                   label:'Hot',
-                   price:'4.99',
-                   description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-                   comment: ''
-                },
-                {
-                   name:'Zucchipakoda',
-                   image: 'images/zucchipakoda.png',
-                   category: 'appetizer',
-                   label:'',
-                   price:'1.99',
-                   description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-                   comment: ''
-                },
-                {
-                   name:'Vadonut',
-                   image: 'images/vadonut.png',
-                   category: 'appetizer',
-                   label:'New',
-                   price:'1.99',
-                   description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
-                   comment: ''
-                },
-                {
-                   name:'ElaiCheese Cake',
-                   image: 'images/elaicheesecake.png',
-                   category: 'dessert',
-                   label:'',
-                   price:'2.99',
-                   description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-                   comment: ''
-                }
-                ];
-            $scope.toggleDetails = function() {
-            $scope.showDetails = !$scope.showDetails;
-            };
+        $scope.isSelected = function (checkTab) {
+            return ($scope.tab === checkTab);
+        };
 
-            //IMPORTANT: NOW, I AM GOING OT ASSIGNTHIS DISHES OBJECT TO THIS CONTROLLER
-//            the dishes variable that i created (var = dishes) will now be assigned to
-//            this dishes object for the menu control
-            // FROM MY OWN UNDERSTANDING: you can now use the dishes variable to define this object :)
-            // this dishes object will be avaiblable to my HTML code later.
+        $scope.toggleDetails = function() {
+        $scope.showDetails = !$scope.showDetails;
+        };
 
-            // $scope.dishes = dishes;
+        //IMPORTANT: NOW, I AM GOING OT ASSIGNTHIS DISHES OBJECT TO THIS CONTROLLER
+    //            the dishes variable that i created (var = dishes) will now be assigned to
+    //            this dishes object for the menu control
+        // FROM MY OWN UNDERSTANDING: you can now use the dishes variable to define this object :)
+        // this dishes object will be avaiblable to my HTML code later.
 
-        }])
+        // $scope.dishes = dishes;
 
-        .controller('ContactController', ['$scope', function($scope) {
-            $scope.feedback = {mychannel:"",
-                              firstName:"",
-                              lastName:"",
-                              agree:false,
-                              email:""};//javascript object
-            $scope.channels = [{value:"tel",  label:"Tel."},
-                               {value:"Email",label:"Email"}]; //Javascript Object Array used to store into javascript object my channel variable
-            $scope.invalidChannelSelection = false; //Javascript boolean
-        }])
+    }])
 
-        .controller('FeedbackController', ['$scope', function($scope) {
-            $scope.sendFeedback = function() { //Javascript Function
+    .controller('ContactController', ['$scope', function($scope) {
+        $scope.feedback = {mychannel:"",
+                          firstName:"",
+                          lastName:"",
+                          agree:false,
+                          email:""};//javascript object
+        $scope.channels = [{value:"tel",  label:"Tel."},
+                           {value:"Email",label:"Email"}]; //Javascript Object Array used to store into javascript object my channel variable
+        $scope.invalidChannelSelection = false; //Javascript boolean
+    }])
+
+    .controller('FeedbackController', ['$scope', function($scope) {
+        $scope.sendFeedback = function() { //Javascript Function
+            console.log($scope.feedback);
+
+            if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+                $scope.invalidChannelSelection = true;
+                console.log('incorrect');
+            }
+            else {
+                $scope.invalidChannelSelection = false;
+                $scope.feedback = {mychannel:"",
+                                   firstName:"",
+                                   lastName:"",
+                                   agree:false,
+                                   email:"" };
+                $scope.feedback.mychannel="";
                 console.log($scope.feedback);
+                $scope.feedbackForm.$setPristine(); //$setPristine is angular way of reseting form, you can do it manually resetting .feedback values
+                console.log($scope.feedback);
+            }
+        };
+    }])
+    .controller('DishDetailController', ['$scope', function($scope) {
 
-                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
-                    $scope.invalidChannelSelection = true;
-                    console.log('incorrect');
-                }
-                else {
-                    $scope.invalidChannelSelection = false;
-                    $scope.feedback = {mychannel:"",
-                                       firstName:"",
-                                       lastName:"",
-                                       agree:false,
-                                       email:"" };
-                    $scope.feedback.mychannel="";
-                    console.log($scope.feedback);
-                    $scope.feedbackForm.$setPristine(); //$setPristine is angular way of reseting form, you can do it manually resetting .feedback values
-                    console.log($scope.feedback);
-                }
+        var dish={
+                      name:'Uthapizza',
+                      image: 'images/uthapizza.png',
+                      category: 'mains',
+                      label:'Hot',
+                      price:'4.99',
+                      description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                       comments: [
+                           {
+                               rating:5,
+                               comment:"Imagine all the eatables, living in conFusion!",
+                               author:"John Lemon",
+                               date:"2012-10-16T17:57:28.556094Z"
+                           },
+                           {
+                               rating:4,
+                               comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                               author:"Paul McVites",
+                               date:"2014-09-05T17:57:28.556094Z"
+                           },
+                           {
+                               rating:3,
+                               comment:"Eat it, just eat it!",
+                               author:"Michael Jaikishan",
+                               date:"2015-02-13T17:57:28.556094Z"
+                           },
+                           {
+                               rating:4,
+                               comment:"Ultimate, Reaching for the stars!",
+                               author:"Ringo Starry",
+                               date:"2013-12-02T17:57:28.556094Z"
+                           },
+                           {
+                               rating:2,
+                               comment:"It's your birthday, we're gonna party!",
+                               author:"25 Cent",
+                               date:"2011-12-02T17:57:28.556094Z"
+                           }
+
+                       ]
+                };
+
+        $scope.dish = dish;
+
+    }])
+    .controller('DishCommentController', ['$scope', function($scope) {
+
+            //Step 1: Create a JavaScript object to hold the comment from the form
+            $scope.mycomment =  {yourname: "",
+                                 yourrating: "",
+                                 yourcomment: ""
             };
-        }]);
+
+            $scope.submitComment = function () {
+
+                //Step 2: This is how you record the date
+                // "The date property of your JavaScript object holding the comment" = new Date().toISOString();
+
+                // Step 3: Push your comment into the dish's comment array
+                $scope.dish.comments.push("Your JavaScript Object holding the comment");
+
+                //Step 4: reset your form to pristine
+
+                //Step 5: reset your JavaScript object that holds your comment
+            }
+        }])
+
+
+
+;
